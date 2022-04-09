5d62cbc257135f955a296bcce636618d1748953b
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-09 21:24:48 +0900

    First commit

---
+++ src/day1/Sample102.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Sample102 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 数値の表示
+		System.out.print(123);
+		System.out.println(456);
+		// 文字列の表示
+		System.out.print("ABC");
+		System.out.println("DEF");
+		// 日本語の表示
+		System.out.print("日本語");
+		System.out.println("を表示");
+	
+	}
+
+}
